b2f8bb290671fc6ab9e85571beb19e18b59e0238
ikestrella <116619952+ikestrella@users.noreply.github.com>
2023-04-12 17:01:35 -0500

    Add files via upload

---
+++ DEMO2/Mensaje.java
@@ -0,0 +1,7 @@
+package DEMO2;
+
+public class Mensaje {
+    public static void main(String[] args) {
+        System.out.println("Loja, una ciudad llena de arte y naturaleza con 400322");
+    }
+}
